662aed6819cb79f97f290003b66a5e528a84b16c
Jayavardhan-J <89264319+Jayavardhan-J@users.noreply.github.com>
2023-05-17 22:37:45 +0530

    Update Main.java

--- src/examplePackage/Main.java
+++ src/examplePackage/Main.java
@@ -1,1 +1,10 @@
+package examplePackage;
 
+public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("Demo");
+        String name=obj.getName();
+        System.out.println(name);
+    }
+}
