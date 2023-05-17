c3ec59db57ab3c69f805c16b15bc214c2c9abde0
Jayavardhan-J <89264319+Jayavardhan-J@users.noreply.github.com>
2023-05-17 22:38:16 +0530

    Update RWOnly.java

--- src/examplePackage/RWOnly.java
+++ src/examplePackage/RWOnly.java
@@ -1,1 +1,12 @@
+package examplePackage;
+
+public class RWOnly {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+}
 
